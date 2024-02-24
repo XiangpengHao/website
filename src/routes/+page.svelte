a93d4ae0f665619733634a7a6d95c4bad35e726e
Xiangpeng Hao <me@haoxp.xyz>
2024-02-24 14:06:19 -0600

    update layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,137 +6,146 @@
 	let paper_open = false;
 </script>
 
-<div style="display: flex; flex-wrap: wrap; justify-content:center;">
-	<h1>Xiangpeng Hao</h1>
-	<h1>(he/him, pronunciations: Shyang-pung How)</h1>
-	<div style="margin-left: 0.5em;">Last update: Jan 2024.</div>
-</div>
-<div style="display: flex; margin-top: 1em; flex-wrap: wrap; justify-content: center;">
-	<div class="card">
-		<img src={portrait} alt="Xiangpeng on Rainer Mountain" style="width: 25em;" />
-		<p style="margin: -0.3em;">
-			(Mt. Rainier, <Tooltip content="hard" theme="hxp">skyline loop trail.</Tooltip>)
-		</p>
-		<div style="max-width:100%;display: flex; justify-content: space-between; margin-top:1em;">
-			<div><a href="https://www.overleaf.com/read/ymxxygrjthhs#9631dc">CV</a></div>
-			<div><a href="https://t.me/life_xiangpeng">Personal life (Chinese)</a></div>
-			<div><a href="https://github.com/XiangpengHao">GitHub</a></div>
-			<div><a href="https://www.linkedin.com/in/hao-xiangpeng/">LinkedIn</a></div>
+<div>
+	<div style="width: 1000px;">
+		<div style="display: flex; flex-wrap: wrap; justify-content:center; margin-bottom:1em;">
+			<h1>Xiangpeng Hao</h1>
+			<h1>(he/him, pronunciations: Shyang-pung How)</h1>
+			<div style="margin-left: 1px;">Last update: Feb 2024.</div>
 		</div>
-	</div>
-	<div class="card" style="width: 30em; line-height: 140%">
-		I'm a third year PhD student at the University of Wisconsin-Madison studying computer science
-		with a focus on <a href="http://www.jfsowa.com/ikl/Stonebraker.pdf">database/storage systems</a
-		>.<br />
-		<br />
-		I enjoy <Tooltip
-			content="Write code, and build system people can use, not just describe algorithms/protocols"
-			theme="hxp"><strong>building</strong></Tooltip
-		>
-		<Tooltip content="Not LLM!" theme="hxp">data-intensive systems.</Tooltip>
-		I've worked on multiple database storage engines and <Tooltip
-			theme="hxp"
-			content="aka, B-tree, LSM-tree, hash table, and ART (adaptive radix tree)."
-		>
-			in-memory/larger-than-memory key-value stores for the past few years.</Tooltip
-		> <br />
-		<br />
-		I aim to <a href="http://herpolhode.com/rob/utah2000.pdf">build systems</a> that have <Tooltip
-			theme="hxp"
-			content="Not for next x years, but now! Not rely on anyone else to read and implement it, but I do it myself!"
-			><strong>direct impact</strong></Tooltip
-		> on people who use the system. I'm serious about building <Tooltip
-			theme="hxp"
-			content="Fearless high performance">reliable systems</Tooltip
-		>: I <Tooltip theme="hxp" content="Sometimes C# if you pay as much as M$">(almost)</Tooltip> exclusively
-		write in
-		<strong>Rust</strong>, I fuzz test all the core components, I run systematic concurrency tests
-		on all my multi-thread code.
-		<br />
-		<br />
 
-		I care about <Tooltip
-			theme="hxp"
-			content="Not (just) academic achievements, money, big houses, etc."
-			><strong>people</strong></Tooltip
-		>.
-		<br />
-		<a href="https://www.cs.sfu.ca/~tzwang/">Tianzheng Wang</a> was my undergraduate advisor. He is
-		a rock-star database researcher, I learned a lot from him. He introduced me to the database
-		research, and I still love it.
-		<br />
-		<a href="https://pages.cs.wisc.edu/~yxy/">Xiangyao Yu</a> was my PhD advisor for the first three
-		and a half years.
-		<br />
-		<a href="https://www.cs.cmu.edu/~yixinluo/">Yixin Luo</a> was my intern mentor @Google. We did
-		great work on database auto-tuning. Sad that I can't continue participating in the project due
-		to external restrictions.
-		<br />
-		<a href="https://www.microsoft.com/en-us/research/people/badrishc/">Badrish Chandramouli</a> was
-		my intern mentor @MSR. He is a great researcher and mentor. He reasons about what I said, and
-		his attrition to details is incredible.
-		<br />
+		<div style="display: flex;">
+			<div class="card" style="line-height: 140%; width: 700px;">
+				I'm a third year PhD student at the University of Wisconsin-Madison studying computer
+				science with a focus on <a href="http://www.jfsowa.com/ikl/Stonebraker.pdf"
+					>database/storage systems</a
+				>.<br />
+				<br />
+				I enjoy <Tooltip
+					content="Write code, and build system people can use, not just describe algorithms/protocols"
+					theme="hxp"><strong>building</strong></Tooltip
+				>
+				<Tooltip content="Not LLM!" theme="hxp">data-intensive systems.</Tooltip>
+				I've worked on multiple database storage engines and <Tooltip
+					theme="hxp"
+					content="aka, B-tree, LSM-tree, hash table, and ART (adaptive radix tree)."
+				>
+					in-memory/larger-than-memory key-value stores for the past few years.</Tooltip
+				> <br />
+				<br />
+				I aim to <a href="http://herpolhode.com/rob/utah2000.pdf">build systems</a> that have <Tooltip
+					theme="hxp"
+					content="Not for next x years, but now! Not rely on anyone else to read and implement it, but I do it myself!"
+					><strong>direct impact</strong></Tooltip
+				> on people who use the system. I'm serious about building <Tooltip
+					theme="hxp"
+					content="Fearless high performance">reliable systems</Tooltip
+				>: I <Tooltip theme="hxp" content="Sometimes C# if you pay as much as M$">(almost)</Tooltip>
+				exclusively write in
+				<strong>Rust</strong>, I fuzz test all the core components, I run systematic concurrency
+				tests on all my multi-thread code.
+				<br />
+				<br />
 
-		Many many other people have helped me along the way. I'm grateful for all of them.
-	</div>
-	<div class="card" style="width: 30em; line-height: 140%;">
-		While <Tooltip theme="hxp" content="... and being a reasonable person">building systems</Tooltip
-		><Tooltip theme="hxp" content="Not the reverse!">,</Tooltip>I <Tooltip
-			theme="hxp"
-			content="Depends on luck and collaborators!">publish papers.</Tooltip
-		>
+				I care about <Tooltip
+					theme="hxp"
+					content="Not (just) academic achievements, money, big houses, etc."
+					><strong>people</strong></Tooltip
+				>.
+				<br />
+				<a href="https://www.cs.sfu.ca/~tzwang/">Tianzheng Wang</a> was my undergraduate advisor. He
+				is a rock-star database researcher, I learned a lot from him. He introduced me to the
+				database research, and I still love it.
+				<br />
+				<a href="https://pages.cs.wisc.edu/~yxy/">Xiangyao Yu</a> was my PhD advisor for the first
+				three and a half years.
+				<br />
+				<a href="https://www.cs.cmu.edu/~yixinluo/">Yixin Luo</a> was my intern mentor @Google. We
+				did great work on database auto-tuning. Sad that I can't continue participating in the
+				project due to external restrictions.
+				<br />
+				<a href="https://www.microsoft.com/en-us/research/people/badrishc/">Badrish Chandramouli</a>
+				was my intern mentor @MSR. He is a great researcher and mentor. He reasons about what I said,
+				and his attrition to details is incredible.
+				<br />
 
-		<CollapsibleCard open={paper_open}>
-			<u slot="header">Click to show.</u>
-			<div slot="body" style="max-height: 30em; overflow: scroll; ">
-				<p>
-					Towards Buffer Management with Tiered Main Memory.<br />
-					<strong>Xiangpeng Hao</strong>, Xinjing Zhou, Xiangyao Yu, Michael Stonebraker. SIGMOD
-					2024, to appear.
-				</p>
-				<p>
-					PiBench Online: Interactive Benchmarking of Persistent Memory Indexes (Demo). <br
-					/><strong>Xiangpeng Hao</strong>, Lucas Lersch, Tianzheng Wang, Ismail Oukid. 45th
-					International Conference on Very Large Data Bases (VLDB 2020)
-				</p>
-				<p>
-					DASH: Dynamic and Scalable Hashing on Persistent Memory.<br /> Baotong Lu,
-					<strong>Xiangpeng Hao</strong>, Tianzheng Wang, Eric Lo. 45th International Conference on
-					Very Large Data Bases (VLDB 2020)
-				</p>
-				<p>
-					Evaluating Persistent Memory based Range Indexes.<br /> Lucas Lersch,
-					<strong>Xiangpeng Hao</strong>, Ismail Oukid, Tianzheng Wang, Thomas Willhalm. 45th
-					International Conference on Very Large Data Bases (VLDB 2020)
-				</p>
-				<p>
-					Evaluating Colour Constancy on the new MIST dataset of Multi-Illuminant Scenes.<br />
-					<strong>Xiangpeng Hao</strong>, Brian Funt, Hanxiao Jiang. 27th Color Image Conference
-				</p>
-				<p>
-					A Multi-illuminant Synthetic Image Test Set.<br /> <strong>Xiangpeng Hao</strong>, Brian
-					Funt. Color Research and Application
-				</p>
+				Many many other people have helped me along the way. I'm grateful for all of them.
 			</div>
-		</CollapsibleCard>
+			<div class="card" style="width: 300px;">
+				<img src={portrait} class="pure-img" alt="Xiangpeng on Mt. Rainier" style="width: 100%;" />
+				<p style="margin: -0.3em;">
+					(Mt. Rainier, <Tooltip content="hard" theme="hxp">skyline loop trail.</Tooltip>)
+				</p>
+				<div style="max-width:100%;display: flex; justify-content: space-between; margin-top:1em;">
+					<div><a href="https://www.overleaf.com/read/ymxxygrjthhs#9631dc">CV</a></div>
+					<div><a href="https://t.me/life_xiangpeng">Personal life (Chinese)</a></div>
+					<div><a href="https://github.com/XiangpengHao">GitHub</a></div>
+					<div><a href="https://www.linkedin.com/in/hao-xiangpeng/">LinkedIn</a></div>
+				</div>
+			</div>
+		</div>
 
-		<hr />
-		<div>
-			Design axioms (in order of importance)
-			<ol>
-				<li>
-					<Tooltip
-						theme="hxp"
-						content="As you may see, I'm not optimized for paper publishing, but I hope this unoptimized version of me can publish enough to graduate."
-						>People-centric.</Tooltip
-					> We build the system for people to use/build-upon/contribute, not just for academic records.
-					We consider usability, fail-safe, documentation while designing the system.
-				</li>
-				<li>Correctness.</li>
-				<li>
-					<Tooltip theme="hxp" content="To publish and to graduate">High performance</Tooltip>
-				</li>
-			</ol>
+		<div style="display: flex; margin-top: 1em; ">
+			<div class="card" style="line-height: 140%; width: 700px;">
+				While <Tooltip theme="hxp" content="... and being a reasonable person"
+					>building systems</Tooltip
+				><Tooltip theme="hxp" content="Not the reverse!">,</Tooltip>I <Tooltip
+					theme="hxp"
+					content="Depends on luck and collaborators!">publish papers.</Tooltip
+				>
+
+				<CollapsibleCard open={paper_open}>
+					<u slot="header">Click to show.</u>
+					<div slot="body" style="max-height: 30em; overflow: scroll; ">
+						<p>
+							Towards Buffer Management with Tiered Main Memory.<br />
+							<strong>Xiangpeng Hao</strong>, Xinjing Zhou, Xiangyao Yu, Michael Stonebraker. SIGMOD
+							2024, to appear.
+						</p>
+						<p>
+							PiBench Online: Interactive Benchmarking of Persistent Memory Indexes (Demo). <br
+							/><strong>Xiangpeng Hao</strong>, Lucas Lersch, Tianzheng Wang, Ismail Oukid. 45th
+							International Conference on Very Large Data Bases (VLDB 2020)
+						</p>
+						<p>
+							DASH: Dynamic and Scalable Hashing on Persistent Memory.<br /> Baotong Lu,
+							<strong>Xiangpeng Hao</strong>, Tianzheng Wang, Eric Lo. 45th International Conference
+							on Very Large Data Bases (VLDB 2020)
+						</p>
+						<p>
+							Evaluating Persistent Memory based Range Indexes.<br /> Lucas Lersch,
+							<strong>Xiangpeng Hao</strong>, Ismail Oukid, Tianzheng Wang, Thomas Willhalm. 45th
+							International Conference on Very Large Data Bases (VLDB 2020)
+						</p>
+						<p>
+							Evaluating Colour Constancy on the new MIST dataset of Multi-Illuminant Scenes.<br />
+							<strong>Xiangpeng Hao</strong>, Brian Funt, Hanxiao Jiang. 27th Color Image Conference
+						</p>
+						<p>
+							A Multi-illuminant Synthetic Image Test Set.<br /> <strong>Xiangpeng Hao</strong>,
+							Brian Funt. Color Research and Application
+						</p>
+					</div>
+				</CollapsibleCard>
+			</div>
+
+			<div class="card" style="width:300px;">
+				Design axioms (in order of importance)
+				<ol>
+					<li>
+						<Tooltip
+							theme="hxp"
+							content="I hope this sub-optimized version of me can publish enough to graduate."
+							>People-centric.</Tooltip
+						> We build the system for people to use/build-upon/contribute, not just for academic records.
+						We consider usability, fail-safe, documentation while designing the system.
+					</li>
+					<li>Correctness.</li>
+					<li>
+						<Tooltip theme="hxp" content="To publish and to graduate">High performance</Tooltip>
+					</li>
+				</ol>
+			</div>
 		</div>
 	</div>
 </div>
