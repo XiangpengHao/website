663b5d64c42ac41e240a48621e7af45407b99a61
Xiangpeng Hao <me@haoxp.xyz>
2025-08-18 14:57:23 -0500

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,13 +9,13 @@
 
 <div class="flex justify-center items-center min-h-screen p-4 bg-gray-100">
 	<div class="max-w-[1000px] w-full bg-white p-2 md:p-4 shadow-sm rounded">
-		<div class="flex flex-wrap justify-center mb-8">
+		<div class="flex flex-wrap justify-center mb-2">
 			<div class="w-full text-center">
 				<h1 class="text-3xl md:text-4xl font-semibold mb-2">Xiangpeng Hao</h1>
-				<p class="text-lg md:text-xl text-gray-700 mb-4">
+				<p class="text-lg md:text-xl text-gray-700 mb-1">
 					(he/him, pronunciations: Shyang-pung How)
 				</p>
-				<div class="text-sm md:text-base text-gray-600 space-y-2 md:space-y-0">
+				<div class="text-sm md:text-base text-gray-600">
 					<a href="{base}/life" class="block md:inline md:ml-6 text-blue-600">Non-academic</a>
 					<a href="{base}/work-with-me" class="block md:inline md:ml-6 text-blue-600"
 						>Work with me</a
@@ -25,81 +25,105 @@
 		</div>
 
 		<div class="flex flex-col md:flex-row">
-			<div class="card w-full md:w-[700px] leading-normal">
+			<div class="card w-full leading-normal">
 				<div class="text-cyan-800 mb-4" style="font-family: Helvetica, Arial, sans-serif">
 					<strong
 						><a href="https://github.com/XiangpengHao/liquid-cache">
 							<img src={LiquidCache} class="h-16 inline" alt="LiquidCache" /></a
-						>: distributed S3 cache for DataFusion-based systems.
+						>: distributed pushdown caching for DataFusion.
 					</strong>
 				</div>
-				I'm a fifth year PhD student at the University of Wisconsin-Madison studying computer science
-				with a focus on database/storage systems.
-				<br />
-				I'm advised by
-				<a href="https://pages.cs.wisc.edu/~dusseau/">Andrea Arpaci-Dusseau</a>
-				and <a href="https://pages.cs.wisc.edu/~remzi/">Remzi H. Arpaci-Dusseau</a>.
-				<div class="mt-2">
-					<strong>
-						<a href="{base}/fund">Fund my PhD:</a>
-					</strong>
-				</div>
-				<div class="funding-table">
-					<table class="w-full border-collapse border border-gray-300">
-						<thead>
-							<tr class="bg-gray-100">
-								<th class="border border-gray-300 px-4 py-1 text-left">Academic year</th>
-								<th class="border border-gray-300 px-4 py-1 text-left">Funding sources</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<td class="border border-gray-300 px-4 py-1">2025-2026</td>
-								<td class="border border-gray-300 px-4 py-1">
-									<div class="flex flex-wrap gap-4 items-center">
-										<a href="https://www.bauplanlabs.com">BauPlan</a>
-										<a href="https://www.influxdata.com"> InfluxData </a>
-										<a href="https://spiraldb.com">Spiral</a>
-										<!-- <a href="https://www.titletowntech.com">TitleTownTech</a> -->
-									</div>
-								</td>
-							</tr>
-							<tr>
-								<td class="border border-gray-300 px-4 py-1">2024-2025</td>
-								<td class="border border-gray-300 px-4 py-1">
-									<a href="https://www.influxdata.com"> InfluxData </a>
-								</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-				<div class="mt-2">
-					My research focuses on building
-					<a href="https://github.com/XiangpengHao/liquid-cache">LiquidCache</a>, a caching
-					infrastructure designed for the next decade of cloud-native analytical systems.
+				<div class="flex flex-col md:flex-row">
+					<div class="md:basis-[70%] md:max-w-[70%] md:pr-6">
+						<p>
+							I'm a fifth year PhD student at the University of Wisconsin-Madison studying computer
+							science with a focus on database/storage systems.
+						</p>
+						<p>
+							I'm advised by
+							<a href="https://pages.cs.wisc.edu/~dusseau/">Andrea Arpaci-Dusseau</a>
+							and <a href="https://pages.cs.wisc.edu/~remzi/">Remzi H. Arpaci-Dusseau</a>.
+						</p>
+						<p>
+							My research focuses on building
+							<a href="https://github.com/XiangpengHao/liquid-cache">LiquidCache</a>, a caching
+							infrastructure designed for the next decade of cloud-native analytical systems.
+						</p>
+
+						<div class="mt-4">
+							<div>
+								<strong>
+									<a href="{base}/fund">Funding I raised for my PhD:</a>
+								</strong>
+							</div>
+							<div class="funding-table">
+								<table class="w-full border-collapse border border-gray-300">
+									<thead>
+										<tr class="bg-gray-100">
+											<th class="border border-gray-300 px-4 py-1 text-left">Academic year</th>
+											<th class="border border-gray-300 px-4 py-1 text-left">Funding sources</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td class="border border-gray-300 px-4 py-1">2025-2026</td>
+											<td class="border border-gray-300 px-4 py-1">
+												<div class="flex flex-wrap gap-4 items-center">
+													<a href="https://www.bauplanlabs.com">BauPlan</a>
+													<a href="https://www.influxdata.com"> InfluxData </a>
+													<a href="https://spiraldb.com">Spiral</a>
+													<!-- <a href="https://www.titletowntech.com">TitleTownTech</a> -->
+												</div>
+											</td>
+										</tr>
+										<tr>
+											<td class="border border-gray-300 px-4 py-1">2024-2025</td>
+											<td class="border border-gray-300 px-4 py-1">
+												<a href="https://www.influxdata.com"> InfluxData </a>
+											</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+					<div class="md:basis-[30%] md:max-w-[30%]">
+						<img src={portrait} class="w-full md:w-4/5 mx-auto" alt="Xiangpeng on Mt. Rainier" />
+						<div class="flex flex-wrap justify-between mt-4 gap-2">
+							<div><a href={cv}>CV</a></div>
+							<div><a href="https://blog.xiangpeng.systems">Blog</a></div>
+							<div class="flex items-center gap-2">
+								<a href="https://t.me/life_xiangpeng">Life</a>
+								<a href="https://aoli.al"
+									><img src={lgbtFlag} class="logo-large" alt="LGBT flag" /></a
+								>
+							</div>
+							<div><a href="https://github.com/XiangpengHao">GitHub</a></div>
+							<div><a href="https://www.linkedin.com/in/hao-xiangpeng/">LinkedIn</a></div>
+						</div>
+					</div>
 				</div>
 
 				<div class="mt-4">
 					People I have worked closely:
 					<br />
-					<a href="https://www.cs.sfu.ca/~tzwang/">Tianzheng Wang</a> -- my undergraduate advisor.
-					He is a rock-star database researcher, I learned a lot from him. He introduced me to the
-					database research, and I still love it.
+					<a href="https://www.cs.sfu.ca/~tzwang/">Tianzheng Wang</a>: my undergraduate advisor. He
+					is a rock-star database researcher.
 					<br />
-					<a href="https://pages.cs.wisc.edu/~yxy/">Xiangyao Yu</a> -- my PhD advisor for the first
+					<a href="https://pages.cs.wisc.edu/~yxy/">Xiangyao Yu</a>: my PhD advisor for the first
 					two and a half years.
 					<br />
-					<a href="https://www.cs.cmu.edu/~yixinluo/">Yixin Luo</a> -- my intern mentor @Google. We
+					<a href="https://www.cs.cmu.edu/~yixinluo/">Yixin Luo</a>: my intern mentor @Google. We
 					did great work on database auto-tuning.
 					<br />
 					<a href="https://www.microsoft.com/en-us/research/people/badrishc/"
 						>Badrish Chandramouli</a
 					>
-					-- my intern mentor @MSR. He is a great researcher and
+					: my intern mentor @MSR. He is a great researcher and
 					<a href="https://blog.xiangpeng.systems/posts/good-advisor/">mentor</a>. He reasons what I
 					said, and his attention to details is incredible.
 					<br />
-					<a href="http://andrew.nerdnetworks.org">Andrew Lamb</a> -- my
+					<a href="http://andrew.nerdnetworks.org">Andrew Lamb</a>: my
 					<a href="https://blog.xiangpeng.systems/posts/working-at-influxdata/">intern</a>
 					mentor @InfluxData. His passion and professionalism in DataFusion development have reshaped
 					my research to connect more closely to real-world applications.
@@ -107,19 +131,6 @@
 				</div>
 
 				<!-- Many other people helped me along the way, I'm grateful for all of them. -->
-			</div>
-			<div class="card w-full md:w-[300px]">
-				<img src={portrait} class="w-full" alt="Xiangpeng on Mt. Rainier" />
-				<div class="flex flex-wrap justify-between mt-4 gap-2">
-					<div><a href={cv}>CV</a></div>
-					<div><a href="https://blog.xiangpeng.systems">Blog</a></div>
-					<div class="flex items-center gap-2">
-						<a href="https://t.me/life_xiangpeng">Life</a>
-						<a href="https://aoli.al"><img src={lgbtFlag} class="logo-large" alt="LGBT flag" /></a>
-					</div>
-					<div><a href="https://github.com/XiangpengHao">GitHub</a></div>
-					<div><a href="https://www.linkedin.com/in/hao-xiangpeng/">LinkedIn</a></div>
-				</div>
 			</div>
 		</div>
 
@@ -134,7 +145,7 @@
 								<li>
 									I co-created and maintain <a href="https://github.com/XiangpengHao/liquid-cache"
 										>LiquidCache</a
-									>, a distributed S3 cache for DataFusion-based systems.
+									>, a distributed pushdown caching for DataFusion.
 								</li>
 								<li>I actively contribute to Apache DataFusion, Arrow, and Parquet.</li>
 							</ol>
